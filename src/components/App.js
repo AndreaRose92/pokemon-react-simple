11a4ba6502961d6471eb763761b4210e5afe91c3
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-06 17:16:39 -0700

    asdfasdfasdf

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
+import logo from '../assets/pokemon-logo-png-1444.png'
+import LandingPage from './LandingPage';
+import Team from './Team';
 import NavBar from './NavBar';
+import PokeDex from './PokeDex'
+import TallGrass from './TallGrass'
 
 function App() {
 
   return (
     <div>
-      <NavBar />
+      <img className='logo' src={logo} alt='logo' />
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/pokedex' element={<PokeDex />} />
+        <Route path='/explore' element={<TallGrass />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,15 @@
 
-import logo from '../assets/pokemon-logo-png-1444.png'
+import { NavLink } from 'react-router-dom'
+
 import '../styles/NavBar.css'
 
 export default function NavBar() {
     return (
-        <div>
-            <img className='logo' src={logo} alt='logo' />
+        <div className='navbar'>
+            <NavLink to='/team' className='tomhanks'>Team</NavLink>
+            <NavLink to='/explore' className='tomhanks'>Explore</NavLink>
+            <NavLink to='/pokedex' className='tomhanks'>PokeDex</NavLink>
+            {/* <NavLink to='' className='navlink'></NavLink> */}
         </div>
     )
 }
